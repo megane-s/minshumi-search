6633a97df92bda0b424d6fdd0bfb9eacf1653398
TBSten <81161390+TBSten@users.noreply.github.com>
2024-01-18 02:51:21 +0900

    環境変数を修正

--- db/engine.py
+++ db/engine.py
@@ -6,8 +6,7 @@ from sqlalchemy import create_engine
 
 @cache
 def get_engine():
-    db_uri = os.environ['DATABASE_URL'] \
-        .replace("postgresql://", "cockroachdb://")
+    db_uri = os.environ['DATABASE_URL']
     engine = create_engine(
         db_uri, connect_args={
             "application_name": "docs_simplecrud_sqlalchemy",
