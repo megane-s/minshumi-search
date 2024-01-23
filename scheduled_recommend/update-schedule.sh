aa398aa558031e2e9b47128dfa109c12afef43f5
TBSten <81161390+TBSten@users.noreply.github.com>
2024-01-23 18:32:35 +0900

    タイムゾーンを修正

--- scheduled_recommend/update-schedule.sh
+++ scheduled_recommend/update-schedule.sh
@@ -3,4 +3,5 @@ gcloud scheduler jobs update http scheduled_recommend \
     --schedule="0 18 * * *" \
     --uri="https://asia-northeast1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/megane-s-gcp/jobs/minshumi-notification:run" \
     --http-method POST \
-    --oauth-service-account-email 174482454094-compute@developer.gserviceaccount.com
+    --oauth-service-account-email 174482454094-compute@developer.gserviceaccount.com \
+    --time-zone=Asia/Tokyo
