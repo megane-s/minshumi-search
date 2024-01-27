ab284f7178c8c3603fa1af8fbdf72f1852460869
TBSten <81161390+TBSten@users.noreply.github.com>
2024-01-27 17:37:02 +0900

    ログの修正

--- searcher/build/__main__.py
+++ searcher/build/__main__.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 from searcher.build.setup_index import setup_index
 from util.log import WithLog
 
-with WithLog("scheduled_recommend.build"):
+with WithLog("searcher.build"):
     load_dotenv()
     setup_index()
