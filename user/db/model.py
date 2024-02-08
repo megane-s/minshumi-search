ab782252a876103cf007d7c7ee0e4629ba500a76
TBSten <81161390+TBSten@users.noreply.github.com>
2024-02-09 03:46:24 +0900

    ArtAppealとWatchingArtを追加

--- user/db/model.py
+++ user/db/model.py
@@ -5,8 +5,9 @@ from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
+from art.db.model import DbArt
 from db.base import Base
-from user.type import Notification, User
+from user.type import ArtAppeal, Notification, User
 
 # DBの定義は以下を参照
 # https://github.com/megane-s/minshumi-frontend/blob/main/prisma/schema.prisma
@@ -19,12 +20,16 @@ class DbUser(Base):
     name = Column(String, nullable=False)
 
     interestTags = relationship("DbInterestTag", backref="parent", lazy=False)
+    artAppeals = relationship("DbArtAppeal", backref="parent", lazy=False)
+    watchingArts = relationship("DbWatchingArt", backref="parent", lazy=False)
 
     def to_user(self):
         return User(
             id=self.id,  # type: ignore
             name=self.name,  # type: ignore
             interest_tags=[row.tag for row in self.interestTags],
+            art_appeals=[row.to_art_appeal() for row in self.artAppeals],
+            watching_arts=[art for art in self.watchingArts]
         )
 
 
@@ -32,6 +37,26 @@ class DbInterestTag(Base):
     __tablename__ = "InterestTag"
     userId = Column(String, ForeignKey(DbUser.id), primary_key=True)
     tag = Column(String, primary_key=True)
+
+
+class DbArtAppeal(Base):
+    __tablename__ = "ArtAppeal"
+    userId = Column(String, ForeignKey(DbUser.id), primary_key=True)
+    artId = Column(String, ForeignKey(DbArt.artId), primary_key=True)
+    likePoint = Column(String, nullable=False)
+
+    def to_art_appeal(self):
+        return ArtAppeal(
+            art_id=self.artId,  # type: ignore
+            user_id=self.userId,  # type: ignore
+            like_point=self.likePoint,  # type: ignore
+        )
+
+
+class DbWatchingArt(Base):
+    __tablename__ = "ArtAppeal"
+    userId = Column(String, ForeignKey(DbUser.id), primary_key=True)
+    artId = Column(String, ForeignKey(DbArt.artId), primary_key=True)
 
 
 class DbNotification(Base):
--- user/type.py
+++ user/type.py
@@ -1,12 +1,23 @@
 
 from dataclasses import dataclass
 
+from art.type import Art
+
+
+@dataclass
+class ArtAppeal:
+    art_id: str
+    user_id: str
+    like_point: str
+
 
 @dataclass
 class User:
     id: str
     name: str
     interest_tags: list[str]
+    art_appeals: list[ArtAppeal]
+    watching_arts: list[Art]
 
 
 @dataclass
