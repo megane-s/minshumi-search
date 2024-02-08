7a8408640d11e9e27f409f157ced1e4eda94781d
TBSten <81161390+TBSten@users.noreply.github.com>
2024-02-09 03:46:45 +0900

    作品アピールや今見ている作品もレコメンド時に使用するように

--- user/recommend/recommend.py
+++ user/recommend/recommend.py
@@ -17,20 +17,34 @@ def get_recommendations(user: str | User, limit: int = 5):
 
     recommend_results: dict[str, GetRecommendArtResultItem] = {}
 
-    # TODO 作品アピールや今見ている作品による探索
-
-    # 興味のあるタグによる探索
-    for tag in user.interest_tags:
+    for item in recommend_iter(user):
         if len(recommend_results) >= limit:
             break
+        if has_recommend_notifications(user.id, item.art.art_id):
+            continue
+        recommend_results[item.art.art_id] = item
+
+    return recommend_results
+
+
+def recommend_iter(user: User):
+    for appeal in user.art_appeals:
+        recommend_result = get_recommend_art_by_art_id(appeal.art_id)
+        if recommend_result is None:
+            continue
+        for art in recommend_result:
+            yield art
+
+    for tag in user.interest_tags:
         recommend_result = get_recommend_art_by_tag(tag)
         if recommend_result is None:
             continue
-        for item in recommend_result:
-            if len(recommend_results) >= limit:
-                break
-            if has_recommend_notifications(user.id, item.art.art_id):
-                continue
-            recommend_results[item.art.art_id] = item
+        for art in recommend_result:
+            yield art
 
-    return recommend_results
+    for art in user.watching_arts:
+        recommend_result = get_recommend_art_by_art_id(art.art_id)
+        if recommend_result is None:
+            continue
+        for art in recommend_result:
+            yield art
