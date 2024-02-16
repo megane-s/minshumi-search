417e4fe336f62cda483318e4206641ded685ae0b
TBSten <81161390+TBSten@users.noreply.github.com>
2024-02-16 13:12:42 +0900

    定期的なレコメンドを金曜日と土曜日に飛ばすように変更

--- scheduled_recommend/update-schedule.sh
+++ scheduled_recommend/update-schedule.sh
@@ -1,6 +1,6 @@
 gcloud scheduler jobs update http scheduled_recommend \
     --location asia-northeast1 \
-    --schedule="0 18 * * *" \
+    --schedule="0 18 * * FRI,SAT" \
     --uri="https://asia-northeast1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/megane-s-gcp/jobs/minshumi-notification:run" \
     --http-method POST \
     --oauth-service-account-email 174482454094-compute@developer.gserviceaccount.com \
